7125a01d0d3284bca2d1a56727bd1f2ee050c569
ZhuK <127308032+ZhuK112@users.noreply.github.com>
2023-06-29 22:18:32 +0300

    123

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Форма</title>
+</head>
+<body>
+    <form method="post" action="script.php">
+        <label for="e">Рядок 1:</label>
+        <input type="text" name="e" id="e" required><br>
+
+        <label for="f">Рядок 2:</label>
+        <input type="text" name="f" id="f" required><br>
+
+        <input type="submit" value="Вивести">
+    </form>
+</body>
+</html>
